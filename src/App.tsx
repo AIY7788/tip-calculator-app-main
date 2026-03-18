f25cf1b5145e527921f92802ee0366ed3c3b3f3d
AIY7788 <aiykhounnakhon@gmail.com>
2026-03-18 09:27:39 +0700

    Updated max width for card

--- src/App.tsx
+++ src/App.tsx
@@ -8,7 +8,7 @@ const App = () => {
       <div className="flex justify-center py-10">
         <Logo />
       </div>
-      <div className="max-w-3xl bg-gray-50 p-5 rounded-t-2xl sm:flex gap-4 md:rounded-2xl">
+      <div className="max-w-2xl bg-gray-50 p-5 rounded-t-2xl sm:flex gap-4 md:rounded-2xl">
         <form action="" className="flex flex-col gap-5 sm:gap-7 sm:p-4">
           <BillInput />
           <SelectTip />
